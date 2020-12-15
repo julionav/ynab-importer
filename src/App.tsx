9dd4cbaf4578777f5decf8d659c468095863d1c6
Julio Navarro <jnavarro@able.co>
2020-12-15 15:17:45 -0500

    Add basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,20 @@ function App() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+    <div className="bg-green-400 min-h-screen font-sans">
+      <div className="pt-20">
+        <div
+          className="mx-auto bg-white rounded w-1/2 max-w-3xl flex h-64 justify-center items-center"
+          {...getRootProps()}
+        >
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
